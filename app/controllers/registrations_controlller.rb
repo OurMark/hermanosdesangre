91c48b20142968946ba1272c3eaece2f90cf6926
Tebayoso <tebayoso@gmail.com>
2014-08-23 16:53:48 -0300

    Finished registrations

---
+++ app/controllers/registrations_controlller.rb
@@ -0,0 +1,14 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :birth, :bloodtype, :dni, :gender, :city, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :birth, :bloodtype, :dni, :gender, :city, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+end
