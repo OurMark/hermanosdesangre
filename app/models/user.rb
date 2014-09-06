f003221fbd3331cc1d4251c6dbfd302cff2c853e
Tebayoso <tebayoso@gmail.com>
2014-09-06 17:14:02 -0300

    Added ong button

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ActiveRecord::Base
   has_many :challenges, through: :ongs
   has_and_belongs_to_many :bookings
   has_and_belongs_to_many :badges
+  validates_presence_of :first_name
 end
