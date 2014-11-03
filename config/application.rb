ac35add33d7e39a46c966ce8fa975b80ad60c1e1
Jorge de los Santos <jdelossantos@tribune.com>
2014-11-03 13:15:23 -0300

    Fixed asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Bloodbrothers
   class Application < Rails::Application
-    config.assets.precompile += [
-                                    'custom.css.scss'
-                                ]
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
-      html_tag
-    }
+    config.assets.precompile += ['custom.css', 'application.css', 'mobile.css']
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
   end
 end
