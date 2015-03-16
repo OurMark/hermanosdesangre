b390ab86ac101598ebd25b2011879a3f3d480ca6
Tebayoso <tebayoso@gmail.com>
2015-03-16 14:17:43 -0300

    Removed migrations

--- db/migrate/20140823013119_create_join_table_user_ong.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableUserOng < ActiveRecord::Migration
-  def change
-    create_join_table :users, :ongs do |t|
-      t.belongs_to :user
-      t.belongs_to :ong
-    end
-  end
-end
