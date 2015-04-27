9fb299a00a3f723179e0558df094555c2449adbc
Jorge de los Santos <tebayoso@gmail.com>
2015-04-27 13:30:33 -0300

    Added current_user validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :check_user_info, except: [:edit]
+  before_filter :check_user_info, unless: :devise_controller?
   before_filter :set_user_relations
 
   protected
 
   def check_user_info
-    redirect_to edit_user_registration_path unless current_user.valid?
+    if current_user && !current_user.valid?
+      flash[:notice] = 'Debes completar todos los campos obligatorios para continuar'
+      redirect_to edit_user_registration_path
+    end
   end
 
   def set_user_relations
