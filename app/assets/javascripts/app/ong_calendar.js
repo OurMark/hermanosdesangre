2ca7852db0a9fa2f29b5f28a4428d83c149710bb
aotaduy <aotaduy@users.noreply.github.com>
2015-09-16 17:42:31 -0300

    Configurar Turnos inital commit

---
+++ app/assets/javascripts/app/ong_calendar.js
@@ -0,0 +1,9 @@
+
+(function($) {
+      $(document).on('ready', function() {
+        $('#agregar-horarios-diferenciados').bind('click', function () {
+            $('#horarios-diferenciados').toggleClass('hidden');
+        });
+
+    });
+}(window.jQuery));
