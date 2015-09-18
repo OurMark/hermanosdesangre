aeef167ac9cb5b87d0dd08d181447f6926866099
aotaduy <aotaduy@users.noreply.github.com>
2015-09-18 16:21:59 -0300

    Work in progress

--- app/assets/javascripts/app/ong_calendar.js
+++ app/assets/javascripts/app/ong_calendar.js
@@ -1,9 +1,20 @@
 
 (function($) {
       $(document).on('ready', function() {
-        $('#agregar-horarios-diferenciados').bind('click', function () {
-            $('#horarios-diferenciados').toggleClass('hidden');
+        $('#accion-horarios-diferenciados').bind('click', function () {
+            $('.horarios-diferenciados').toggleClass('hidden');
         });
 
+        $('.accion-toggle-horario-cortado').bind('click', function () {
+            $('.horario-cortado').toggleClass('hidden');
+            $('#input_horario_cortado').val($('#input_horario_cortado').val() === '0' ? '1' : '0');
+        });
+        $('input[type="checkbox"].dayEnabled').bind('click', function (event) {
+        console.log(event);
+            if (event.target.checked === true) {
+                $('.from-to.'  + event.target.name).toggleClass('hidden');
+            }
+            // body...
+        });
     });
 }(window.jQuery));
