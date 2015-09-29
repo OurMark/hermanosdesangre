8791ce1b3b59b4f349ff55fc65860b0d61f03264
Carolina Iungelson <c.iungelson@globant.com>
2015-09-29 16:20:53 -0300

    Arreglo formulario y validacion js de Nuevo usuario

--- app/assets/javascripts/app/registration.js
+++ app/assets/javascripts/app/registration.js
@@ -1,47 +1,60 @@
 (function($) {
     $('[data-toggle="tooltip"]').tooltip();
 
-  $('.btn-registrar').on('click', function( event ) {
-    validarContraseñas();
-    validarDni();
-    validarEmail();
-    //falta que se quede en la pagina y no siga
+  $('.registrarse form').on('submit', function( event ) {
+    var contraseniaValida = validarContrasenias();
+    var dniValido = validarDni();
+    var emailValido = validarEmail();
+    return contraseniaValida && dniValido && emailValido;
   })
 
-  function validarContraseñas(){
-    if ( $('#user_password').val() == $('#user_password_confirmation').val() ){
-      $('#user_password_confirmation').tooltip('hide');
+  function validarContrasenias(){
+    var passwordConfirmation = $('#user_password_confirmation');
+    var userPassword = $('#user_password');
+    if ( userPassword.val() == '' ){
+      passwordConfirmation.attr('data-original-title', 'Campo de contraseña vacío');
+      passwordConfirmation.tooltip('show');
+      return false;
     }else{
-      $('#user_password_confirmation').tooltip('show');
+     if ( userPassword.val() === passwordConfirmation.val() ){
+      passwordConfirmation.tooltip('hide');
+      return true;
+     }else{
+      passwordConfirmation.attr('data-original-title', 'Las contraseñas ingresadas no son iguales');
+      passwordConfirmation.tooltip('show');
+      return false;
+    }
     }
   }
 
   function validarDni(){
     if ((/^[0-9]{8}$/).test($('#user_user_detail_attributes_dni').val()) ){
       $('#user_user_detail_attributes_dni').tooltip('hide');
+      return true;
     }else{
       $('#user_user_detail_attributes_dni').tooltip('show');
+      return false;
     }
   }
 
   function validarEmail(){
     if ((/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test( $('#user_username').val())){
       $('#user_username').tooltip('hide');
+      return true;
     }else{
       $('#user_username').tooltip('show');
+      return false;
     }
   }
 
-  $( "#user_user_detail_attributes_dni" ).blur(function() {
-    validarDni();
-  });
+  $( "#user_user_detail_attributes_dni" ).blur( validarDni );
 
   $( "#user_username" ).blur(function() {
     validarEmail();
   });
 
   $( "#user_password, #user_password_confirmation").blur(function() {
-    validarContraseñas();
+    validarContrasenias();
   });
 
 }(window.jQuery));
