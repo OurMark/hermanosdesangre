4dfeea48de9d78f11b7502c110414c40083ebfa8
Carolina Iungelson <c.iungelson@globant.com>
2015-10-02 17:25:06 -0300

    Mejoras en el codigo fuente

--- app/assets/javascripts/app/registration.js
+++ app/assets/javascripts/app/registration.js
@@ -15,17 +15,16 @@
       passwordConfirmation.attr('data-original-title', 'Campo de contraseña vacío');
       passwordConfirmation.tooltip('show');
       return false;
-    }else{
-     if ( userPassword.val() === passwordConfirmation.val() ){
+    }
+    if ( userPassword.val() === passwordConfirmation.val() ){
       passwordConfirmation.tooltip('hide');
       return true;
-     }else{
-      passwordConfirmation.attr('data-original-title', 'Las contraseñas ingresadas no son iguales');
-      passwordConfirmation.tooltip('show');
-      return false;
     }
-    }
+    passwordConfirmation.attr('data-original-title', 'Las contraseñas ingresadas no son iguales');
+    passwordConfirmation.tooltip('show');
+    return false;
   }
+
 
   function validarDni(){
     if ((/^[0-9]{8}$/).test($('#user_user_detail_attributes_dni').val()) ){
@@ -47,13 +46,13 @@
     }
   }
 
-  $( "#user_user_detail_attributes_dni" ).blur( validarDni );
+  $('#user_user_detail_attributes_dni').blur( validarDni );
 
-  $( "#user_username" ).blur(function() {
+  $('#user_username').blur(function() {
     validarEmail();
   });
 
-  $( "#user_password, #user_password_confirmation").blur(function() {
+  $('#user_password, #user_password_confirmation').blur(function() {
     validarContrasenias();
   });
 
