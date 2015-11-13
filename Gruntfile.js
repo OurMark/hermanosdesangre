fe71071806b8a37468fd71f477f5e07f5513ac17
fernando fajardo <fernando.fajardo@globant.com>
2015-11-13 16:30:04 -0500

    Add grunt js and optimization tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,18 +14,75 @@ module.exports = function(grunt) {
     global_vars: global_vars,
     pkg: grunt.file.readJSON('package.json'),
 
+    // image optimization task
+    imagemin: {
+      dynamic: {
+       files: [{
+         expand: true,
+         cwd: 'app/assets/images/',
+         src: ['**/*.{png,jpg,svg}'],
+         dest: 'app/assets/images/'
+       }]
+      }
+    },
+
+    // files clean task
+    clean: [
+      'app/assets/javascripts/hermanos.concat.js'
+    ],
+
+    // js concatenation
+    concat: {
+      dist: {
+        src: ['app/assets/javascripts/app/*.js'],
+        dest: 'app/assets/javascripts/hermanos.concat.js',
+      }
+    },
+
+    // js minification
+    uglify: {
+      hemanosjs: {
+        files: {
+          'app/assets/javascripts/hermanos.min.js': ['app/assets/javascripts/hermanos.concat.js']
+        }
+      }
+    },
+
+    // js validations task
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
       all: [
         'Gruntfile.js',
-        '<%= global_vars.base_path %>' + '<%= global_vars.js %>' + '/**/*.js'
+        '<%= global_vars.base_path %>' + '<%= global_vars.js %>' + '/app/*.js'
       ]
     },
 
+    //sass compilation
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'app/assets/sass',
+          cssDir: 'app/assets/stylesheets',
+          environment: 'development'
+        }
+      }
+    },
+
+    // grunt watch task
     watch: {
-      grunt: { files: ['Gruntfile.js'] },
+
+      grunt: {
+        files: ['Gruntfile.js']
+      },
+
+      sass: {
+        files: [
+          '<%= global_vars.base_path %>' + '<%= global_vars.sass %>' + '/**/*.scss'
+        ],
+        tasks: ['compass']
+      },
 
       js: {
         files: [
@@ -36,5 +93,12 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.registerTask('default', ['jshint', 'watch']);
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'clean', 'watch']);
+  grunt.registerTask('sass', ['compass']); //sass files compilation task
+  grunt.registerTask('image', ['imagemin']); //image optimization task
 };
