b67cd1dfec22292cd28657af38d0cbc92bed8eaf
fernando fajardo <fernando.fajardo@globant.com>
2016-02-17 15:30:24 -0500

    ong calendar save action fixed

--- app/controllers/ongs_controller.rb
+++ app/controllers/ongs_controller.rb
@@ -7,8 +7,8 @@ class OngsController < ApplicationController
   def index
     @ongs = Topic.blood_donation.first.ongs
     respond_to do |format|
-        format.html
-        format.json { render json: @ongs  }
+      format.html
+      format.json { render json: @ongs  }
     end
   end
 
@@ -92,49 +92,74 @@ class OngsController < ApplicationController
   end
 
   def calendar
-      @ong = Ong.find(params[:ong_id])
-      @days = ['LU', 'MA', 'MI' , 'JU', 'VI', 'SA', 'DO']
-      @intervalo_horario = []
-    (00..23).each {|hour|
-        ['00', '30'].each {|minutes|
-         @intervalo_horario.append(hour.to_s + ':' + minutes)
-         }
-     }
-      @duracion_turnos = ['30', '45', '60']
+    @ong = Ong.find(params[:ong_id])
+    @ong_calendars = @ong.ong_calendars
+    @days = ['LU', 'MA', 'MI' , 'JU', 'VI', 'SA', 'DO']
+    @intervalo_horario = []
+    (00..23).each do |hour|
+      ['00', '30'].each do |minutes|
+        @intervalo_horario.append(hour.to_s + ':' + minutes)
+      end
+    end
+    @duracion_turnos = ['30', '45', '60']
   end
 
   def save_calendar
     @days = [:LU, :MA, :MI, :JU, :VI, :SA, :DO]
     logger.info 'Find ong'
     @ong = Ong.find(params[:ong_id])
-    # @ong.ong_detail.update(beds: params[:camas], timelapse: params[:duracion])
-    @days.each { |day|
-      logger.info 'Find or create day #{day}'
-      calendar = @ong.ong_calendars.find_or_create_by(day: day)
 
-      calendar.start_time = params[day][:inicio]
-      calendar.end_time = params[day][:hasta]
-      calendar.day = day
-      calendar.save
-    }
+    @days.each do |day|
+      # if all days param is set
+      unless params[:dayall].nil?
+        calendar = @ong.ong_calendars.find_or_create_by(day: day)
+        calendar.start_time = params[:ALL][:inicio]
+        calendar.end_time = params[:ALL][:hasta]
+        calendar.start_time_differential = params[:ALL][:inicio_cortado]
+        calendar.end_time_differental = params[:ALL][:hasta_cortado]
+        calendar.day = day
+        calendar.save
+      else
+        # if not set all days param and we select specific
+        # hours for each day
+        if params[:ALL][:inicio] == '0:00' and params[:ALL][:hasta] == '0:00'
+          calendar = @ong.ong_calendars.find_or_create_by(day: day)
+          calendar.start_time = params[day][:inicio]
+          calendar.end_time = params[day][:hasta]
+          calendar.start_time_differential = params[day][:inicio_cortado]
+          calendar.end_time_differental = params[day][:hasta_cortado]
+          calendar.day = day
+          calendar.save
+        else
+          # if not set all days params but we select the same
+          # hours for the selected days
+          calendar = @ong.ong_calendars.find_or_create_by(day: day)
+          calendar.start_time = params[:ALL][:inicio]
+          calendar.end_time = params[:ALL][:hasta]
+          calendar.start_time_differential = params[:ALL][:inicio_cortado]
+          calendar.end_time_differental = params[:ALL][:hasta_cortado]
+          calendar.day = day
+          calendar.save
+        end
+      end
+    end
     respond_to do |format|
       format.html { redirect_to user_path(current_user), notice: 'Calendario guardado' }
       format.json { head :no_content }
     end
-
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ong
-      @ong = Ong.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_ong
+    @ong = Ong.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def ong_params
-      params.require(:ong).permit(:name, :address, :address2, :city, :addressState,
-                                  :country, :zip, :country, :phone, :adminEmail,
-                                  :websiteUrl, :facebookPage, :nationalNetworkName,
-                                  :internationalNetworkName, :comments)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def ong_params
+    params.require(:ong).permit(:name, :address, :address2, :city, :addressState,
+                                :country, :zip, :country, :phone, :adminEmail,
+                                :websiteUrl, :facebookPage, :nationalNetworkName,
+                                :internationalNetworkName, :comments)
+  end
 end
