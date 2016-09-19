0cdbe449c6fbb1cd7e6304c55202d86ecd861941
Maria E. Chacon <maria.chacon@globant.com>
2016-09-19 10:44:38 -0300

    Ver Turnos: Fix errors on field names

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ActiveRecord::Base
   belongs_to :ong
   belongs_to :user
   scope :most_near, -> {where("date >= ?", Date.today)}
-  scope :last_done, -> {where("date <= ?", Date.today).order("day desc")}
+  scope :last_done, -> {where("date <= ?", Date.today).order("date desc")}
 end
