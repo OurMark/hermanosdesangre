bca4e737d25f0a5078475fc328b6fa55ad0a25cf
Maria E. Chacon <maria.chacon@globant.com>
2016-09-30 09:46:55 -0300

    add new bookings, refactoring

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ActiveRecord::Base
   belongs_to :user
   scope :most_near, -> {where("date >= ?", Date.today)}
   scope :last_done, -> {where("date <= ?", Date.today).order("date desc")}
+
 end
